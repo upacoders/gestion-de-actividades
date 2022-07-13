7af5739546e3bce21b526bc5facd2b03e4072124
MarianaNeu <mariana.neumann@upa.edu.py>
2022-07-13 11:46:22 -0400

    agregue cambios en menu a todas las pantallas

--- src/pantallas/planificacion.java
+++ src/pantallas/planificacion.java
@@ -9,12 +9,12 @@ package pantallas;
  *
  * @author fabrizioriveros
  */
-public class planificación extends javax.swing.JFrame {
+public class planificacion extends javax.swing.JFrame {
 
     /**
      * Creates new form planificación
      */
-    public planificación() {
+    public planificacion() {
         initComponents();
     }
 
@@ -201,20 +201,23 @@ public class planificación extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(planificación.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(planificacion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(planificación.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(planificacion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(planificación.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(planificacion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(planificación.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(planificacion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new planificación().setVisible(true);
+                new planificacion().setVisible(true);
             }
         });
     }
